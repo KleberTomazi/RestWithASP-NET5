f08e1fbbda5196992881b5b5c49d8e7a4cb73199
Kleber Tomazi <kleberluiz9@gmail.com>
2021-01-29 08:26:40 -0300

    Adotando uma Arquitetura em Camadas

---
+++ 06_RestWithASPNET_AdoptingLayeredArchitecture/RestWithASPNET/RestWithASPNET/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -0,0 +1,103 @@
+﻿using RestWithASPNET.Models;
+using RestWithASPNET.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Repository.Implementations
+{
+    public class PersonRepositoryImplementation : IPersonRepository
+    {
+        private MySQLContext _context;
+
+        public PersonRepositoryImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        // Method responsible for returning all "persons"
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        // Method responsible for returning a person
+        public Person FindByID(long id)
+        {
+            {
+                return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            }
+        }
+
+        // Method responsible for creating a new person
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return person;
+        }
+
+        // Method responsible for updating a person 
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return person;
+        }
+
+        // Method responsible for deleting a person from an ID
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
+            {
+
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        // Method to validate if the ID exists
+        public bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+    }
+}
