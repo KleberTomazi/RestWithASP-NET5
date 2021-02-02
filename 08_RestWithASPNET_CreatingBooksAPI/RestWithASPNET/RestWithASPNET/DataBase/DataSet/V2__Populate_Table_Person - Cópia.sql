311ce9368bbcbb56bb50977187163b1db2311288
Kleber Tomazi <kleberluiz9@gmail.com>
2021-02-02 08:20:43 -0300

    Implementando uma nova API (books) com os principais verbos

---
+++ 08_RestWithASPNET_CreatingBooksAPI/RestWithASPNET/RestWithASPNET/DataBase/DataSet/V2__Populate_Table_Person - Cópia.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Airton', 'Male', 'Senna'),
+	(2, 'Jundiaí -  São Paulo - Brasil', 'Kleber', 'Male', 'Tomazi'),
+	(3, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(4, 'Kentucky - USA', 'Mohamed', 'Male', 'Ali'),
+	(5, 'Jundiaí - São Paulo - Brasil', 'Kleiton', 'Male', 'Tomazi'),
+	(6, 'Tatuapé - São Paulo - Brasil', 'Carlos ', 'Male', 'Resende'),
+	(7, 'Rio de Janeiro - Brasil', 'Leonardo', 'Male', 'Sodré ');
