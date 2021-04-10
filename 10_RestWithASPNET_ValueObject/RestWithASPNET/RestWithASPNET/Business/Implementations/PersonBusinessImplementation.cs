c1a03b474db67d65c48e8bf8fde085bf6d2bd884
Kleber Tomazi <kleberluiz9@gmail.com>
2021-04-09 21:12:34 -0300

    Implementando o padrão de projetos Value Object

---
+++ 10_RestWithASPNET_ValueObject/RestWithASPNET/RestWithASPNET/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,58 @@
+﻿using RestWithASPNET.Data.Converter.Implementations;
+using RestWithASPNET.Data.VO;
+using RestWithASPNET.Models;
+using RestWithASPNET.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+
+        // AS REGRAS DE NEGÓCIO E AS VALIDAÇÕES DEVEM SER APLICADAS AQUI!
+
+        private readonly IRepository<Person> _repository;
+
+        private readonly PersonConverter _converter;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+            _converter = new PersonConverter();
+        }
+
+        // Method responsible for returning all "persons"
+        public List<PersonVO> FindAll()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        // Method responsible for returning a person
+        public PersonVO FindByID(long id)
+        {
+            return _converter.Parse(_repository.FindByID(id));
+        }
+
+        // Method responsible for creating a new person
+        public PersonVO Create(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person); //Preciso "parsear" o objeto que chega (PersonVO) para poder persisti-lo na base
+            personEntity = _repository.Create(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        // Method responsible for updating a person 
+        public PersonVO Update(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person); //Preciso "parsear" o objeto que chega (PersonVO) para poder persisti-lo na base
+            personEntity = _repository.Update(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        // Method responsible for deleting a person from an ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ 10_RestWithASPNET_ValueObject/RestWithASPNET/RestWithASPNET/Data/Converter/Implementations/PersonConverter.cs
@@ -0,0 +1,74 @@
+﻿using RestWithASPNET.Data.Converter.Contract;
+using RestWithASPNET.Data.VO;
+using RestWithASPNET.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNET.Data.Converter.Implementations
+{
+    // Converte PersonVO para Person e Person para PersonVO
+    public class PersonConverter : IParser<PersonVO, Person>, IParser<Person, PersonVO>
+    {
+        public Person Parse(PersonVO origin)
+        {
+            if (origin == null)
+            {
+                return null;
+            }
+            else
+            {
+                return new Person
+                {
+                    Id = origin.Id,
+                    FirstName = origin.FirstName,
+                    LastName = origin.LastName,
+                    Adress = origin.Adress,
+                    Gender = origin.Gender
+                };
+            }
+        }
+
+        public PersonVO Parse(Person origin)
+        {
+            if (origin == null)
+            {
+                return null;
+            }
+            else
+            {
+                return new PersonVO
+                {
+                    Id = origin.Id,
+                    FirstName = origin.FirstName,
+                    LastName = origin.LastName,
+                    Adress = origin.Adress,
+                    Gender = origin.Gender
+                };
+            }
+        }
+
+        public List<Person> Parse(List<PersonVO> origin)
+        {
+            if (origin == null)
+            {
+                return null;
+            }
+            else
+            {
+                return origin.Select(item => Parse(item)).ToList();
+            }
+        }
+
+        public List<PersonVO> Parse(List<Person> origin)
+        {
+            if (origin == null)
+            {
+                return null;
+            }
+            else
+            {
+                return origin.Select(item => Parse(item)).ToList();
+            }
+        }
+    }
+}
